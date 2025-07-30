ba9163628761cb5eac750fbeaba8a50cdd6e696a
unknown <vishisht22868@gmail.com>
2025-07-30 07:09:39 +0530

    Selection sort

---
+++ Vishisht/Sorting/Selection.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Selection {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter size of array: ");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter elements of the array");
+        for(int i=0;i<size;i++){
+            array[i] = sc.nextInt();
+        }
+        int[] newArray = Selection_Sort(array);
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.printf("%d ",newArray[i]);
+        }
+        sc.close();
+    }
+    public static int[] Selection_Sort(int[] array) {
+        int size = array.length;
+        for(int i=0;i<size;i++){
+            int min=array[i];
+            for(int j=i;j<size;j++){
+                if(array[j]<min){
+                    min=array[j];
+                    array[j]=array[i];
+                    array[i]=min;
+                }
+            }
+        }
+        return array;
+    }
+}
