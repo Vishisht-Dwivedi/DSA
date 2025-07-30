b1159c23f616c6265b5014550e644293754f76c2
unknown <vishisht22868@gmail.com>
2025-07-30 07:29:54 +0530

    Optimised bubble

--- Vishisht/Sorting/Bubble.java
+++ Vishisht/Sorting/Bubble.java
@@ -19,13 +19,16 @@ public class Bubble {
     public static int[] Bubble_Sort(int[] array){
         int size = array.length;
         for (int i = 0; i < size; i++) {
+            int swaps = 0;
             for (int j = 0; j < size-i-1; j++) {
                 if(array[j]>array[j+1]){
                     int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
+                    swaps++;
                 }
             }    
+            if(swaps==0) return array;
         }
         return array;
     }
