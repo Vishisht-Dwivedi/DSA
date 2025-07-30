28dd34bd44c5142bf6044c95befe9285c196511c
unknown <vishisht22868@gmail.com>
2025-07-30 07:25:35 +0530

    Bubble sort

--- Vishisht/Sorting/Bubble.java
+++ Vishisht/Sorting/Bubble.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Selection {
-    public static void main(String[] args) {
+public class Bubble {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.printf("Enter size of array: ");
         int size = sc.nextInt();
@@ -10,23 +10,22 @@ public class Selection {
         for(int i=0;i<size;i++){
             array[i] = sc.nextInt();
         }
-        int[] newArray = Selection_Sort(array);
+        int[] newArray = Bubble_Sort(array);
         for (int i = 0; i < newArray.length; i++) {
             System.out.printf("%d ",newArray[i]);
         }
         sc.close();
-    }
-    public static int[] Selection_Sort(int[] array) {
+    }   
+    public static int[] Bubble_Sort(int[] array){
         int size = array.length;
-        for(int i=0;i<size;i++){
-            int min=array[i];
-            for(int j=i;j<size;j++){
-                if(array[j]<min){
-                    min=array[j];
-                    array[j]=array[i];
-                    array[i]=min;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size-i-1; j++) {
+                if(array[j]>array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
                 }
-            }
+            }    
         }
         return array;
     }
