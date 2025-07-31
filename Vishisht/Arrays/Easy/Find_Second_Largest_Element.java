664b1559d968638f352865bdc84c69f9a776398f
unknown <vishisht22868@gmail.com>
2025-07-31 07:26:09 +0530

    Find second largest element in array

---
+++ Vishisht/Arrays/Easy/Find_Second_Largest_Element.java
@@ -0,0 +1,30 @@
+package Vishisht.Arrays.Easy;
+import java.util.Scanner;
+
+public class Find_Second_Largest_Element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter size of array: ");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter elements of the array");
+        for(int i=0;i<size;i++){
+            array[i] = sc.nextInt();
+        }
+        int max = find_second_largest(array);
+        System.out.printf("Second largest element: %d",max);
+        sc.close();
+    }
+    public static int find_second_largest(int[] array){
+        int max = array[0];
+        int secondLargest = array[0];
+        for(int i=0;i<array.length;i++){
+            if(array[i]>max){
+                secondLargest = max;
+                max=array[i];
+            }
+        }
+        return secondLargest;
+    }
+}
+
