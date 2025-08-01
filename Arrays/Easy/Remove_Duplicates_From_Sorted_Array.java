bfe723074c393df7bc6a32811f44760249505da1
unknown <vishisht22868@gmail.com>
2025-08-01 07:31:03 +0530

    Remove duplicates from sorted

---
+++ Arrays/Easy/Remove_Duplicates_From_Sorted_Array.java
@@ -0,0 +1,18 @@
+package Arrays.Easy;
+
+public class Remove_Duplicates_From_Sorted_Array {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+}
