861234b88ef9c65189dad18c228b5f1e3093e584
unknown <vishisht22868@gmail.com>
2025-08-13 06:34:29 +0530

    missing number

---
+++ Arrays/Easy/Missing_Number.java
@@ -0,0 +1,11 @@
+package Arrays.Easy;
+public class Missing_Number {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n*(n+1)/2;
+        for(int i=0;i<n;i++){
+            sum-=nums[i];
+        }       
+        return sum;
+    }
+}
